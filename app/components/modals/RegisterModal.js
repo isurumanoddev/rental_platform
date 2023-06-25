e662eebcd105aa27ef5587078b05182dceeb5be3
isuru <isurumanod99@gmail.com>
2023-06-25 17:09:05 +0530

    Complete user interface for register form

--- app/components/modals/RegisterModal.js
+++ app/components/modals/RegisterModal.js
@@ -8,6 +8,9 @@ import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
 import Modal1 from "@/app/components/modals/Modal1";
 import Heading from "@/app/components/Heading";
 import Input from "@/app/components/Input";
+// import axios from "axios";
+import {Button} from "@mui/material";
+import {Google} from "@mui/icons-material";
 
 
 function RegisterModal() {
@@ -26,6 +29,15 @@ function RegisterModal() {
     })
 
     const onSubmit = (data) => {
+        setIsLoading(true);
+        // axios.post("/api/register", data)
+        //     .then(() => {
+        //         registerState.onClose()
+        //         setIsLoading(false)
+        //     } )
+        //     .catch(errors => {
+        //         console.log(errors)
+        //     })
 
 
     }
@@ -35,9 +47,23 @@ function RegisterModal() {
             <Input register={register}
                    id={"email"}
                    label={"Email"}
-                   type={"text"}
+                   type={"email"}
                    errors={errors}
 
+
+            /> <Input register={register}
+                      id={"name"}
+                      label={"Name"}
+                      type={"text"}
+                      errors={errors}
+
+
+        />
+            <Input register={register}
+                   id={"password"}
+                   label={"Password"}
+                   type={"password"}
+                   errors={errors}
 
 
             />
@@ -45,6 +71,20 @@ function RegisterModal() {
         </div>
 
     )
+    const footerContent = (
+        <div className={"flex flex-col gap-3 mt-3"}>
+            <Button className={" py-2 flex gap-5 outline-none text-white   bg-black text-sm hover:bg-black  "}
+                    startIcon={<Google/>}>
+                Sign in with Google
+            </Button>
+
+            <div className={"mt-4 text-gray-500 flex flex-row gap-2 items-center justify-center"}>
+                <p>Already have an account</p>
+                <p onClick={registerState.onClose} className={"underline cursor-pointer hover:text-gray-800 transition duration-100"}>Login</p>
+            </div>
+        </div>
+    );
+
 
 
     return (
@@ -56,6 +96,7 @@ function RegisterModal() {
             onClose={registerState.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
             actionLabel={"Continue"}/>
 
 
