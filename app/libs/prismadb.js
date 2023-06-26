126528aafbe7b9d024e7b427e6368ce8439efa69
isuru <isurumanod99@gmail.com>
2023-06-26 07:49:54 +0530

    Connect prisma adapter and check user credentials

--- app/libs/prismadb.js
+++ app/libs/prismadb.js
@@ -1,8 +1,7 @@
 import {PrismaClient} from "@prisma/client";
-// const { PrismaClient } = require('@prisma/client')
 
 
-const client = new PrismaClient();
 
 
-export default client;
+const prisma = new PrismaClient()
+export default prisma;
